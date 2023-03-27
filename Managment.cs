d0c8b28a3a2865cd6f87c0d4e7f3df2f4276f4fe
Subhash Barela <barelasubhash16@gmail.com>
2023-03-27 14:42:03 +0530

    Retrive total number of same record using count keyword

--- Managment.cs
+++ Managment.cs
@@ -23,16 +23,31 @@ namespace Day35LinqOperation
       
         public void SelectRecordList(List<ProductReview> review)
         {
-            var recordData = from productReviews in review
-                              where (productReviews.ProductId == 1 && productReviews.Rating > 3) ||
-                              (productReviews.ProductId == 4 && productReviews.Rating > 3) ||
-                              (productReviews.ProductId == 9 && productReviews.Rating > 3) 
+            //var recordData = from productReviews in review
+            //                  where (productReviews.ProductId == 1 && productReviews.Rating > 3) ||
+            //                  (productReviews.ProductId == 4 && productReviews.Rating > 3) ||
+            //                  (productReviews.ProductId == 9 && productReviews.Rating > 3) 
+            //                  select productReviews;
+
+            var recordData1 = from productReviews in review
+                              where (productReviews.ProductId == 1 || productReviews.ProductId == 4 || productReviews.ProductId == 9 && productReviews.Rating > 3)
                               select productReviews;
 
 
-            foreach (var list in recordData)
+            foreach (var list in recordData1)
             {
                 Console.WriteLine("Product Id: " + list.ProductId + " | UserId: " + list.UserId + " | Rating: " + list.Rating + " | Review: " + list.Review + " | Like: " + list.isLike);
+
+            }
+        }
+        public void RetriveCountOfRecordList(List<ProductReview> review)
+        {
+            var recordData = review .GroupBy(x => x.ProductId).Select(x => new {ProductId = x.Key, Count = x.Count()});
+                             
+
+            foreach (var list in recordData)
+            {
+                Console.WriteLine("Product Id: " + list.ProductId + " Total: "+list.Count);
 
             }
         }
--- Program.cs
+++ Program.cs
@@ -19,13 +19,13 @@ namespace Day35LinqOperation
                 new ProductReview() {ProductId =3, UserId=2, Rating=5, Review= "Good", isLike=true},
                 new ProductReview() {ProductId =4, UserId=2, Rating=4, Review= "Good", isLike=true},
                 new ProductReview() {ProductId =5, UserId=3, Rating=2, Review= "Bad", isLike=true},
-                new ProductReview() {ProductId =6, UserId=4, Rating=4, Review= "Good", isLike=true},
+                new ProductReview() {ProductId =2, UserId=4, Rating=4, Review= "Good", isLike=true},
                 new ProductReview() {ProductId =1, UserId=3, Rating=6, Review= "Good", isLike=true},
                 new ProductReview() {ProductId =10, UserId=10, Rating=1, Review= "Bad", isLike=false},
                 new ProductReview() {ProductId =11, UserId=10, Rating=1, Review= "Bad", isLike=false},
-                new ProductReview() {ProductId =12, UserId=10, Rating=2, Review= "Bad", isLike=false},
+                new ProductReview() {ProductId =5, UserId=10, Rating=2, Review= "Bad", isLike=false},
                 new ProductReview() {ProductId =13, UserId=10, Rating=3, Review= "Nice", isLike=true},
-                new ProductReview() {ProductId =14, UserId=10, Rating=4, Review= "Good", isLike=true},
+                new ProductReview() {ProductId =5, UserId=10, Rating=4, Review= "Good", isLike=true},
                 new ProductReview() {ProductId =9, UserId=10, Rating=5, Review= "Good", isLike=true},
                 new ProductReview() {ProductId =16, UserId=10, Rating=3, Review= "Nice", isLike=true}
             };
@@ -34,7 +34,9 @@ namespace Day35LinqOperation
 
             Managment management = new Managment();
            // management.TopRecordList(productReviews);
-            management.SelectRecordList(productReviews);
+           // management.SelectRecordList(productReviews);
+            management.RetriveCountOfRecordList(productReviews);
+
 
         }
     }
