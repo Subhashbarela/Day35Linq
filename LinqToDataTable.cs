1cd46b0a494d6f18668ac06a5d758fc66ac9b706
Subhash Barela <barelasubhash16@gmail.com>
2023-03-27 17:07:32 +0530

    create data table class to store a data

---
+++ LinqToDataTable.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day35LinqOperation
+{
+    public class LinqToDataTable
+    {
+        public void AddDataTable()
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("ProductId");
+            dt.Columns.Add("UserId");
+            dt.Columns.Add("Rating");
+            dt.Columns.Add("Review");
+            dt.Columns.Add("isLike");
+
+            dt.Rows.Add("1","1","5", "good", true);
+            dt.Rows.Add("2", "1", "5", "good", true);
+            dt.Rows.Add("3", "1", "6", "bad", true);
+            dt.Rows.Add("4", "2", "5", "good", false);
+
+            DisplayProduct(dt);
+        }
+        public void DisplayProduct(DataTable dt)
+        {
+            var productName = from product in dt.AsEnumerable() select product.Field<string>("productName");
+            Console.WriteLine("productName");
+            foreach(string item in productName)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -12,30 +12,9 @@ namespace Day35LinqOperation
         {
             Console.WriteLine("Welcome to Product Review Management Problem ");
 
-            List<ProductReview> productReviews = new List<ProductReview>()
-            {
-                new ProductReview() {ProductId =1, UserId=1, Rating=5, Review= "Good", isLike=true},
-                new ProductReview() {ProductId =2, UserId=1, Rating=4, Review= "Good", isLike=true},
-                new ProductReview() {ProductId =3, UserId=2, Rating=5, Review= "Good", isLike=true},
-                new ProductReview() {ProductId =4, UserId=2, Rating=4, Review= "Good", isLike=true},
-                new ProductReview() {ProductId =5, UserId=3, Rating=2, Review= "Bad", isLike=true},
-                new ProductReview() {ProductId =6, UserId=4, Rating=4, Review= "Good", isLike=true},
-                new ProductReview() {ProductId =7, UserId=3, Rating=6, Review= "Good", isLike=true},
-                new ProductReview() {ProductId =8, UserId=10, Rating=1, Review= "Bad", isLike=false},
-                new ProductReview() {ProductId =9, UserId=10, Rating=1, Review= "Bad", isLike=false},
-                new ProductReview() {ProductId =10, UserId=10, Rating=2, Review= "Bad", isLike=false},
-                new ProductReview() {ProductId =11, UserId=10, Rating=3, Review= "Nice", isLike=true},
-                new ProductReview() {ProductId =12, UserId=10, Rating=4, Review= "Good", isLike=true},
-                new ProductReview() {ProductId =13, UserId=10, Rating=5, Review= "Good", isLike=true},
-                new ProductReview() {ProductId =14, UserId=10, Rating=3, Review= "Nice", isLike=true}
-            };
-
-           
-
-            Managment management = new Managment();
-          
-         //  management.SelectedRowRecordList(productReviews);
-            management.RetriveRecordUsingEquals(productReviews);
+            LinqToDataTable dt = new LinqToDataTable();
+            dt.AddDataTable();
+           // dt.DisplayProduct();
 
 
 
