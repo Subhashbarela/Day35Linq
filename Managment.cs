8569f1bf947c0464614ce329b425116392f7a800
Subhash Barela <barelasubhash16@gmail.com>
2023-03-27 11:40:10 +0530

    show product review and rating using linq query

---
+++ Managment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day35LinqOperation
+{
+    public class Managment
+    {
+        public void TopRecordList(List<ProductReview> review)
+        {
+            var recordData = (from productReviews in review
+                              orderby productReviews.Rating descending
+                              select productReviews).Take(3);
+
+            foreach (var list in recordData)
+            {
+                Console.WriteLine("Product Id: " + list.ProductId + " | UserId: " + list.UserId + " | Rating: " + list.Rating + " | Review: " + list.Review + " | Like: " + list.isLike);
+
+            }
+        }
+    }
+}
